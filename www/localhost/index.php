bb503de24fc838bb68d802923bedf91dad651403
Krystallalala <mind_lab@163.com>
2021-07-16 23:21:46 +0800

    添加服务容器mysql[8.0.13]/redis[含PHP相关扩展]

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -1,3 +1,20 @@
 <?php
+// echo phpinfo();
+// die();
+echo "Hello: ";
+try {
+    $dbh = new PDO('mysql:host=mysql;dbname=my', 'root', '123456');
+    $sql = "SELECT * FROM users";
+    $res = $dbh->query($sql);
+    echo $res->fetchColumn(3);
+} catch (PDOException $e) {
+    echo "数据库连接失败：". $e->getMessage();
+}
 
-echo "Hello World.";
+//连接本地的 Redis 服务
+$redis = new Redis();
+$redis->connect('redis', 6379);
+echo "Connection to server successfully";
+      //查看服务是否运行
+echo "Server is running: " . $redis->ping();
+
